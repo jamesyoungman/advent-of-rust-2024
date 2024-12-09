c0989628d201b6241b317713bdaada4a9ad80bea
James Youngman <james@youngman.org>
2024-12-09 07:35:59 +0000

    Day 09: merge free-space representation into preceding file

--- src/bin/day09/main.rs
+++ src/bin/day09/main.rs
@@ -6,12 +6,12 @@ use std::str;
 struct File {
     id: u32,
     len: u32,
+    following_space: u32,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct DiskMap {
     files: VecDeque<File>,
-    spaces: VecDeque<u32>,
 }
 
 fn parse_input(s: &str) -> DiskMap {
@@ -26,7 +26,6 @@ fn parse_input(s: &str) -> DiskMap {
 
     let s = s.trim();
     let mut files = VecDeque::new();
-    let mut spaces = VecDeque::new();
     let mut it = s.chars().enumerate();
     loop {
         match it.next() {
@@ -35,22 +34,18 @@ fn parse_input(s: &str) -> DiskMap {
                 files.push_back(File {
                     id,
                     len: parse_char(len),
+                    following_space: match it.next() {
+                        Some((_, space_len)) => parse_char(space_len),
+                        None => u32::MAX,
+                    },
                 });
-            }
-            None => {
-                panic!("missing file length");
-            }
-        }
-        match it.next() {
-            Some((_, space_len)) => {
-                spaces.push_back(parse_char(space_len));
             }
             None => {
                 break;
             }
         }
     }
-    DiskMap { files, spaces }
+    DiskMap { files }
 }
 
 #[test]
@@ -59,11 +54,22 @@ fn test_parse_input_12345() {
         parse_input("12345"),
         DiskMap {
             files: VecDeque::from(vec![
-                File { id: 0, len: 1 },
-                File { id: 1, len: 3 },
-                File { id: 2, len: 5 }
+                File {
+                    id: 0,
+                    len: 1,
+                    following_space: 2
+                },
+                File {
+                    id: 1,
+                    len: 3,
+                    following_space: 4
+                },
+                File {
+                    id: 2,
+                    len: 5,
+                    following_space: u32::MAX
+                }
             ]),
-            spaces: VecDeque::from(vec![2, 4])
         }
     );
 }
@@ -74,32 +80,33 @@ fn test_parse_input_90909() {
         parse_input("90909"),
         DiskMap {
             files: VecDeque::from(vec![
-                File { id: 0, len: 9 },
-                File { id: 1, len: 9 },
-                File { id: 2, len: 9 }
+                File {
+                    id: 0,
+                    len: 9,
+                    following_space: 0
+                },
+                File {
+                    id: 1,
+                    len: 9,
+                    following_space: 0
+                },
+                File {
+                    id: 2,
+                    len: 9,
+                    following_space: u32::MAX
+                }
             ]),
-            spaces: VecDeque::from(vec![0, 0])
         }
     );
 }
 
 impl DiskMap {
-    fn compact(mut self) -> Vec<u32> {
+    fn compact_part1(mut self) -> Vec<u32> {
         let mut result = Vec::new();
-        let mut remaining_space: u32 = 0;
         while let Some(file) = self.files.pop_front() {
+            let mut remaining_space: u32 = file.following_space;
             for _ in 0..file.len {
                 result.push(file.id);
-            }
-            if remaining_space == 0 {
-                if let Some(space) = self.spaces.pop_front() {
-                    if space == 0 {
-                        continue;
-                    }
-                    remaining_space = space;
-                } else {
-                    remaining_space = u32::MAX;
-                }
             }
             while remaining_space > 0 && !self.files.is_empty() {
                 if let Some(mut file) = self.files.pop_back() {
@@ -120,19 +127,19 @@ impl DiskMap {
 }
 
 #[test]
-fn test_compact_90909() {
+fn test_compact_90909_part1() {
     let disk = parse_input("90909");
     assert_eq!(
-        disk.compact(),
+        disk.compact_part1(),
         vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2,]
     );
 }
 
 #[test]
-fn test_compact_12345() {
+fn test_compact_12345_part1() {
     let disk = parse_input("12345"); // file ids 0 (1 block),1 (3 blocks),2 (5 blocks)
     assert_eq!(
-        disk.compact(),
+        disk.compact_part1(),
         vec![
             0, // file 0 (1 block) unmoved
             2, 2, // filling the first (2-block) space
@@ -143,10 +150,10 @@ fn test_compact_12345() {
 }
 
 #[test]
-fn test_compact_2333133121414131402() {
+fn test_compact_2333133121414131402_part1() {
     let disk = parse_input(sample_input());
     assert_eq!(
-        disk.compact(),
+        disk.compact_part1(),
         vec![0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6]
     );
 }
@@ -172,7 +179,7 @@ fn sample_input() -> &'static str {
 }
 
 fn part1(disk: DiskMap) -> u64 {
-    disk_checksum(&disk.compact())
+    disk_checksum(&disk.compact_part1())
 }
 
 #[test]
