6188d52f3f698d2bf5b61ff4cf205d0855e60404
James Youngman <james@youngman.org>
2024-12-11 20:23:55 +0000

    Solved day 11 part 2.

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::str;
 
 use lib::parse::parse_number;
@@ -22,6 +23,7 @@ fn make_two_stones(val: Value, ndigits: usize) -> [Value; 2] {
     [val / divisor, val % divisor]
 }
 
+#[test]
 fn test_make_two_stones() {
     assert_eq!(make_two_stones(10, 2), [1, 0]);
     assert_eq!(make_two_stones(21, 2), [2, 1]);
@@ -55,48 +57,64 @@ fn test_number_of_digits() {
     assert_eq!(number_of_digits(1000), 4);
 }
 
-fn consider_stone(val: Value, output: &mut Vec<Value>) {
-    if val == 0 {
-        output.push(1);
+fn consider_stone_n_times(
+    val: Value,
+    times: usize,
+    memo: &mut HashMap<(Value, usize), usize>,
+) -> usize {
+    if times == 0 {
+        return 1;
+    }
+    let memo_key = (val, times);
+    if let Some(&stone_count) = memo.get(&memo_key) {
+        return stone_count;
+    }
+
+    let stone_count = if val == 0 {
+        consider_stone_n_times(1, times - 1, memo)
     } else {
         let ndigits = number_of_digits(val);
         if ndigits % 2 == 0 {
-            output.extend(make_two_stones(val, ndigits));
+            let stones = make_two_stones(val, ndigits);
+            consider_stone_n_times(stones[0], times - 1, memo)
+                .checked_add(consider_stone_n_times(stones[1], times - 1, memo))
+                .expect("addition should not overflow")
         } else {
             match val.checked_mul(2024) {
-                Some(newval) => output.push(newval),
+                Some(newval) => consider_stone_n_times(newval, times - 1, memo),
                 None => {
                     panic!("{val}*2048 is out-of-range!");
                 }
             }
         }
-    }
+    };
+    memo.insert(memo_key, stone_count);
+    stone_count
 }
 
 #[test]
-fn test_consider_stone() {
-    fn consider(val: Value) -> Vec<Value> {
-        let mut result = Vec::new();
-        consider_stone(val, &mut result);
-        result
+fn test_iterate_stone_once() {
+    fn iterate_stone_once(val: Value) -> usize {
+        let mut memo = HashMap::new();
+        consider_stone_n_times(val, 1, &mut memo)
     }
-    assert_eq!(consider(0), vec![1]);
-    assert_eq!(consider(10), vec![1, 0]);
-    assert_eq!(consider(63), vec![6, 3]);
-    assert_eq!(consider(1234), vec![12, 34]);
-    assert_eq!(consider(3), vec![3 as Value * 2024]);
+    assert_eq!(iterate_stone_once(0), 1);
+    assert_eq!(iterate_stone_once(10), 2);
+    assert_eq!(iterate_stone_once(63), 2);
+    assert_eq!(iterate_stone_once(1234), 2);
+    assert_eq!(iterate_stone_once(3), 1);
 }
 
-fn blink(input: Vec<Value>) -> Vec<Value> {
-    let mut result = Vec::with_capacity(input.len());
-    for stone in input {
-        consider_stone(stone, &mut result);
-    }
-    result
+fn blink(input: &[Value], times: usize) -> usize {
+    let mut memo = HashMap::new();
+    input
+        .iter()
+        .map(|val| consider_stone_n_times(*val, times, &mut memo))
+        .sum()
 }
 
 #[test]
-fn test_blink() {
+fn test_blink_1() {
     let expected = vec![
         vec![125, 17],
         vec![253000, 1, 7],
@@ -109,24 +127,28 @@ fn test_blink() {
             2,
         ],
     ];
-    let mut current = expected[0].clone();
-    for expect in &expected[1..] {
-        current = blink(current);
-        assert_eq!(&current, expect);
+    for i in 1..expected.len() {
+        let got = blink(&expected[i - 1], 1);
+        let expect = expected[i].len();
+        if got != expect {
+            panic!(
+                "wrong stone count for input {:?}: expected {expect}, got {got}",
+                expected[i - 1]
+            );
+        }
     }
 }
 
-fn part1(mut stones: Vec<Value>) -> usize {
-    for _ in 0..25 {
-        stones = blink(stones);
-    }
-    stones.len()
+fn part1(stones: &[Value]) -> usize {
+    blink(stones, 25)
+}
+
+fn part2(stones: &[Value]) -> usize {
+    blink(stones, 75)
 }
 
 fn parse_input(s: &str) -> Vec<Value> {
-    s.split_whitespace()
-        .map(|label| parse_number(label))
-        .collect()
+    s.split_whitespace().map(parse_number).collect()
 }
 
 #[test]
@@ -136,5 +158,6 @@ fn test_parse_input() {
 
 fn main() {
     let input = parse_input(str::from_utf8(include_bytes!("input.txt")).unwrap());
-    println!("day 11 part 1: {}", part1(input));
+    println!("day 11 part 1: {}", part1(&input));
+    println!("day 11 part 2: {}", part2(&input));
 }
